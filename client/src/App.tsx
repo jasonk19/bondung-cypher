42065a20d3e014103eec9815108d3b1051662835
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-02-17 11:24:44 +0700

    feat: template page for cipher pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,36 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Affine from "./pages/Affine";
+import Vigenere from "./pages/Vigenere";
+import ExtendedVigenere from "./pages/ExtendedVigenere";
 import Playfair from "./pages/Playfair";
+import Hill from "./pages/Hill";
+import SuperEncryption from "./pages/SuperEncryption";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <></>
+      path: "/affine",
+      element: <Affine />
+    },
+    {
+      path: "/vigenere",
+      element: <Vigenere />
+    },
+    {
+      path: "/extended-vigenere",
+      element: <ExtendedVigenere />
     },
     {
       path: "/playfair",
       element: <Playfair />
+    },
+    {
+      path: "/hill",
+      element: <Hill />
+    },
+    {
+      path: "/super-encryption",
+      element: <SuperEncryption />
     }
   ]);
 
