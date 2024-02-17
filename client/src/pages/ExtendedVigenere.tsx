6af7ca8db212ac2a34e55e890f657439a0e386ae
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-02-17 22:26:24 +0700

    feat: binary file extended vigenere

--- client/src/pages/ExtendedVigenere.tsx
+++ client/src/pages/ExtendedVigenere.tsx
@@ -1,9 +1,297 @@
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  Divider,
+  HStack,
+  Heading,
+  Icon,
+  Input,
+  Stack,
+  Text,
+  Textarea,
+  VStack,
+  useToast
+} from "@chakra-ui/react";
+import { FaLock, FaLockOpen } from "react-icons/fa";
+import { MdFileDownload } from "react-icons/md";
 import Layout from "../components/Layout";
+import { useState } from "react";
+import axios, { AxiosError } from "axios";
+import CustomFileInput from "../components/CustomFileInput";
 
 export default function ExtendedVigenere() {
+  const toast = useToast();
+  const [key, setKey] = useState("");
+  const [plainText, setPlainText] = useState("");
+  const [cipherText, setCipherText] = useState("");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [plainTextFile, setPlainTextFile] = useState<File>();
+  const [cipherTextFile, setCipherTextFile] = useState<File>();
+
+  const executeEncrypt = async () => {
+    setIsLoading(true);
+    try {
+      if (plainTextFile !== undefined) {
+        const data = new FormData();
+        data.append("mode", "encrypt");
+        data.append("key", key);
+        data.append("file", plainTextFile, plainTextFile.name);
+
+        const response = await axios.post(
+          "http://localhost:3000/api/cipher/extended-vigenere",
+          data,
+          {
+            responseType: "blob"
+          }
+        );
+
+        // Fetch the filename
+        const contentDisposition = response.headers[
+          "content-disposition"
+        ] as string;
+        let filename = "encrypted_file";
+        if (contentDisposition) {
+          const filenameSection = contentDisposition.split(";")[1];
+          if (filenameSection) {
+            filename = filenameSection
+              .trim()
+              .split("filename=")[1]
+              .replace(new RegExp(`"`, "g"), "");
+          }
+        }
+
+        // Generate link for download
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", filename); // Set the downloaded file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        setIsLoading(false);
+      } else {
+        const response = await axios.post(
+          "http://localhost:3000/api/cipher/extended-vigenere",
+          {
+            mode: "encrypt",
+            key: key,
+            text: plainText
+          }
+        );
+        setResult(response.data.result);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast({
+          title: "Encryption error",
+          status: "error",
+          description: error.response?.data.message,
+          position: "top-right"
+        });
+      }
+      setIsLoading(false);
+    }
+  };
+
+  const executeDecrypt = async () => {
+    setIsLoading(true);
+    try {
+      if (cipherTextFile !== undefined) {
+        const data = new FormData();
+        data.append("mode", "decrypt");
+        data.append("key", key);
+        data.append("file", cipherTextFile, cipherTextFile.name);
+
+        const response = await axios.post(
+          "http://localhost:3000/api/cipher/extended-vigenere",
+          data,
+          {
+            responseType: "blob"
+          }
+        );
+
+        // Fetch the filename
+        const contentDisposition = response.headers[
+          "content-disposition"
+        ] as string;
+        let filename = "decrypted_file";
+        if (contentDisposition) {
+          const filenameSection = contentDisposition.split(";")[1];
+          if (filenameSection) {
+            filename = filenameSection
+              .trim()
+              .split("filename=")[1]
+              .replace(new RegExp(`"`, "g"), "");
+          }
+        }
+
+        // Generate link for download
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", filename); // Set the downloaded file name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        setIsLoading(false);
+      } else {
+        const response = await axios.post(
+          "http://localhost:3000/api/cipher/extended-vigenere",
+          {
+            mode: "decrypt",
+            key: key,
+            text: cipherText
+          }
+        );
+        setResult(response.data.result);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast({
+          title: "Decryption error",
+          status: "error",
+          description: error.response?.data.message,
+          position: "top-right"
+        });
+      }
+      setIsLoading(false);
+    }
+  };
+
+  const download = () => {
+    const element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(result)
+    );
+    element.setAttribute("download", "result.txt");
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const handlePlainTextFileChange = (file: File | undefined) => {
+    setPlainTextFile(file);
+  };
+
+  const handleCipherTextFileChange = (file: File | undefined) => {
+    setCipherTextFile(file);
+  };
+
   return (
     <Layout>
-      <></>
+      <VStack
+        w={"full"}
+        py={8}
+        px={32}
+        align={"center"}
+        justifyContent={"space-around"}
+        gap={8}
+      >
+        <HStack w={"50%"} justifyContent={"center"}>
+          <Text>Cipher key</Text>
+          <Input
+            w={"60%"}
+            placeholder="Insert cipher key here"
+            value={key}
+            onChange={(event) => setKey(event.target.value)}
+          />
+        </HStack>
+        <HStack w={"80%"} gap={8}>
+          <Stack w={"full"}>
+            <Text>Extended Vigenere plain text</Text>
+            <Textarea
+              value={plainText}
+              onChange={(event) => setPlainText(event.target.value)}
+              resize={"none"}
+              placeholder="Plain text"
+              rows={5}
+            />
+            <Box position="relative" p={4}>
+              <Divider border={"1px solid black"} />
+              <AbsoluteCenter bg={"main"} px="4">
+                or
+              </AbsoluteCenter>
+            </Box>
+            <CustomFileInput onFileChange={handlePlainTextFileChange} />
+            <Button
+              w={"full"}
+              onClick={executeEncrypt}
+              isLoading={isLoading}
+              isDisabled={isLoading}
+            >
+              <HStack>
+                <Icon as={FaLock} />
+                <Text>
+                  {plainTextFile !== undefined
+                    ? "Encrypt file content"
+                    : "Encrypt"}
+                </Text>
+              </HStack>
+            </Button>
+          </Stack>
+          <Stack w={"full"}>
+            <Text>Extended Vigenere ciphertext</Text>
+            <Textarea
+              value={cipherText}
+              onChange={(event) => setCipherText(event.target.value)}
+              resize={"none"}
+              placeholder="Cipher text"
+              rows={5}
+            />
+            <Box position="relative" p={4}>
+              <Divider border={"1px solid black"} />
+              <AbsoluteCenter bg={"main"} px="4">
+                or
+              </AbsoluteCenter>
+            </Box>
+            <CustomFileInput onFileChange={handleCipherTextFileChange} />
+            <Button
+              w={"full"}
+              onClick={executeDecrypt}
+              isLoading={isLoading}
+              isDisabled={isLoading}
+            >
+              <HStack>
+                <Icon as={FaLockOpen} />
+                <Text>
+                  {cipherTextFile !== undefined
+                    ? "Decrypt file content"
+                    : "Decrypt"}
+                </Text>
+              </HStack>
+            </Button>
+          </Stack>
+        </HStack>
+        <Stack placeSelf={"start"} w={"full"}>
+          <HStack
+            borderBottom={"1px solid black"}
+            py={2}
+            w={"full"}
+            justifyContent={"space-between"}
+          >
+            <Heading fontSize={"x-large"}>Result</Heading>
+            <Button onClick={download} isDisabled={result.length === 0}>
+              <HStack>
+                <Icon as={MdFileDownload} boxSize={6} />
+                <Text>Download as txt</Text>
+              </HStack>
+            </Button>
+          </HStack>
+          {result.length > 0 ? (
+            <Text>{result}</Text>
+          ) : (
+            <Text color={"gray"} fontStyle={"italic"}>
+              Results will be displayed here
+            </Text>
+          )}
+        </Stack>
+      </VStack>
     </Layout>
   );
 }
--- server/app/controllers/api/cipher/extended_vigenere_controller.rb
+++ server/app/controllers/api/cipher/extended_vigenere_controller.rb
@@ -1,41 +1,123 @@
 class Api::Cipher::ExtendedVigenereController < ApplicationController
-  before_action :convert_to_bytes
-  def create
-    unless params[:mode] == "encrypt" or params[:mode] == "decrypt"
-      render json: { message: "Invalid mode" }, status: :unprocessable_entity
-    end
+  before_action :set_key_bytes
 
-    if params[:mode] == "encrypt"
-      encrypted_bytes = encrypt
-      render json: {
-        bytes: encrypted_bytes,
-        result: Base64.encode64(encrypted_bytes.pack('C*')),
-        hex: encrypted_bytes.map { |byte| format("%02x ", byte) }.join,
-        binary: encrypted_bytes.map { |byte| format("%08b ", byte) }.join(' ')
-      }, status: :ok
-    elsif params[:mode] == "decrypt"
-      decrypted_bytes = decrypt
-      render json: { result: decrypted_bytes.pack('C*').force_encoding('UTF-8') }, status: :ok
+  def create
+    return render_invalid_mode unless valid_mode?
+    return render_invalid_key unless key_present?
+
+    if params[:file].present?
+      process_file
+    else
+      set_input_bytes
+      process_text
     end
   end
 
   private
 
-  def convert_to_bytes
-    if params[:mode] == "decrypt"
-      @plaintext_bytes = Base64.decode64(params[:text]).bytes
+  def valid_mode?
+    %w[encrypt decrypt].include?(params[:mode])
+  end
+
+  def key_present?
+    params[:key].present?
+  end
+
+  def render_invalid_mode
+    render json: { message: "Invalid mode" }, status: :unprocessable_entity
+  end
+
+  def render_invalid_key
+    render json: { message: "Key cannot be empty" }, status: :unprocessable_entity
+  end
+
+  def process_file
+    method = "#{params[:mode]}_file"
+    send(method)
+  end
+
+  def encrypt_file
+    encrypted_data, filename = handle_file_encryption(params[:file])
+    send_encrypted_data(encrypted_data, filename)
+  end
+
+  def decrypt_file
+    decrypted_data, filename = handle_file_decryption(params[:file])
+    send_decrypted_data(decrypted_data, filename)
+  end
+
+  def process_text
+    method = "#{params[:mode]}_text"
+    result = send(method, @input_bytes)
+    if params[:mode] == "encrypt"
+      render_encrypted_text(result)
     else
-      @plaintext_bytes = params[:text].bytes
+      render_decrypted_text(result)
     end
+  end
+
+  def set_key_bytes
     @key_bytes = params[:key].bytes
   end
 
-  def encrypt
+  def set_input_bytes
+    @input_bytes = params[:mode] == "decrypt" ? Base64.decode64(params[:text]).bytes : params[:text].bytes
+  end
+
+  def handle_file_encryption(file)
+    content = File.read(file.tempfile, mode: "rb")
+    encrypted_bytes = encrypt_text(content.bytes)
+    metadata_encoded = encode_metadata(file.original_filename)
+    [metadata_encoded + encrypted_bytes.pack("C*"), "Encrypted_#{file.original_filename}"]
+  end
+
+  def handle_file_decryption(file)
+    content, filename = decrypt_file_content(File.read(file.tempfile, mode: "rb"))
+    [content, filename]
+  end
+
+  def send_encrypted_data(data, filename)
+    send_data data, filename: filename, type: "application/octet-stream", disposition: "attachment"
+  end
+
+  def send_decrypted_data(data, filename)
+    send_data data, filename: filename, type: "application/octet-stream", disposition: "attachment"
+  end
+
+  def render_encrypted_text(encrypted_bytes)
+    render json: {
+      bytes: encrypted_bytes,
+      result: Base64.encode64(encrypted_bytes.pack("C*"))
+    }, status: :ok
+  end
+
+  def render_decrypted_text(decrypted_bytes)
+    render json: { result: decrypted_bytes.pack("C*").force_encoding("UTF-8") }, status: :ok
+  end
+
+  def encode_metadata(filename)
+    metadata = { filename: filename }.to_json
+    metadata_encoded = Base64.strict_encode64(metadata)
+    metadata_length = [metadata_encoded.bytesize].pack("N")
+    "#{metadata_length}#{metadata_encoded}"
+  end
+
+  def decrypt_file_content(content)
+    metadata_length = content[0...4].unpack("N").first
+    metadata = JSON.parse(Base64.decode64(content[4...(4+metadata_length)]))
+    original_filename = metadata["filename"]
+
+    encrypted_content = content[(4+metadata_length)..].unpack("C*")
+    decrypted_bytes = decrypt_text(encrypted_content)
+    [decrypted_bytes.pack("C*"), original_filename]
+  end
+
+  def encrypt_text(text)
     encrypted_bytes = []
     key_length = @key_bytes.length
 
-    (0..@plaintext_bytes.length - 1).each do |i|
-      plain_byte = @plaintext_bytes[i]
+    (0..text.length - 1).each do |i|
+      plain_byte = text[i]
       key_byte = @key_bytes[i % key_length]
 
       encrypted_byte = (plain_byte + key_byte) % 256
@@ -43,21 +125,25 @@ class Api::Cipher::ExtendedVigenereController < ApplicationController
       encrypted_bytes.append(encrypted_byte)
     end
 
+    puts encrypted_bytes[0]
+
     encrypted_bytes
   end
 
-  def decrypt
+  def decrypt_text(text)
     decrypted_bytes = []
     key_length = @key_bytes.length
 
-    (0..@plaintext_bytes.length - 1).each do |i|
-      encrypted_byte = @plaintext_bytes[i]
+    (0..text.length - 1).each do |i|
+      encrypted_byte = text[i]
       key_byte = @key_bytes[i % key_length]
 
       decrypted_byte = (encrypted_byte - key_byte + 256) % 256
 
       decrypted_bytes.append(decrypted_byte)
     end
+
+    puts decrypted_bytes[0], "AFTER DECRYPT"
 
     decrypted_bytes
   end
